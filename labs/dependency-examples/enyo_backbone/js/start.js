e462bec1dd3c24f6cfd50c89ae1adadc51da10d5
solojavier <1.27201@gmail.com>
2013-04-25 16:38:00 -0500

    initial commit

--- labs/dependency-examples/enyo_backbone/js/start.js
+++
@@ -1,7 +0,0 @@
-/*jshint strict:false */
-/*global enyo:false, ToDo:false */
-// Once everything is loaded through enyo's dependency management, start the app
-enyo.ready(function () {
-	window.app = new ToDo.Application();
-	window.app.render();
-});
